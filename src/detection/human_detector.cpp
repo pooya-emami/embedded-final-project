9d636562a05ac31ab151e0fcbb6645b17af98fdf
Pooya Emami <pooyaemamivar77@gmail.com>
2026-08-07 16:10:32 +0330

    project updated

--- src/detection/human_detector.cpp
+++ src/detection/human_detector.cpp
@@ -1,4 +1,8 @@
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/dnn.hpp>
+
 #include <iostream>
 #include <chrono>
 #include <sstream>
