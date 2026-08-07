528f1d33e2384d03572e3619dd20fa8511955499
Pooya Emami <pooyaemamivar77@gmail.com>
2026-08-07 19:10:07 +0330

    project updated

--- src/server/server.c
+++ src/server/server.c
@@ -20,7 +20,7 @@
 
 #define BUFFER_SIZE 65536
 #define HTML_PATH "../html/template.html"
-#define CONFIG_PATH "/usr/local/bin/server.conf"
+#define CONFIG_PATH "server.conf"
 
 int g_frame_interval_ms = DEFAULT_FRAME_INTERVAL_MS;
 int g_frame_width = DEFAULT_FRAME_WIDTH;
