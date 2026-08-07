1687eb7462dc348dc3740693219599f00d9a00cb
Pooya Emami <pooyaemamivar77@gmail.com>
2026-08-08 00:36:08 +0330

    project updated

--- src/relay/mjpeg_relay.h
+++ src/relay/mjpeg_relay.h
@@ -4,11 +4,9 @@
 #include <stddef.h>
 
 #define RELAY_PORT     9000
-#define RELAY_BUF_SIZE 65536
+#define RELAY_BUF_SIZE 262144
 
-int find_marker(const unsigned char *buf, size_t len,
-                 unsigned char m1, unsigned char m2);
-
+int find_marker(const unsigned char *buf, size_t len, unsigned char m1, unsigned char m2);
 void *receiver_thread(void *arg);
 
 #endif /* MJPEG_RELAY_H */
