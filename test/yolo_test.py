ae2bb20ff8fb35816c451926b4fdb56b1635591b
Pooya Emami <pooyaemamivar77@gmail.com>
2026-08-08 22:51:57 +0330

    project updated

--- test/yolo_test.py
+++ test/yolo_test.py
@@ -1,17 +1,11 @@
-import cv2
-import numpy as np
+from ultralytics import YOLO
 
-MODEL_PATH = "../models/yolov8n.onnx"
+model = YOLO("yolov8n.pt")
 
-net = cv2.dnn.readNetFromONNX(MODEL_PATH)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
-img = np.zeros((320, 320, 3), dtype=np.uint8)
-blob = cv2.dnn.blobFromImage(img, 1/255.0, (320, 320), swapRB=True)
-net.setInput(blob)
-
-out = net.forward()
-
-print("Python output dims:", out.ndim)
-print("Python output shape:", out.shape)
+model.export(
+    format="onnx",
+    opset=11,
+    simplify=True,
+    dynamic=False,
+    imgsz=320
+)
