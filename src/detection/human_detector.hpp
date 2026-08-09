a7ff48d0218667190d77505372d9b4f7f0dd0ef4
Pooya Emami <pooyaemamivar77@gmail.com>
2026-08-09 04:37:34 +0330

    project updated

--- src/detection/human_detector.hpp
+++ src/detection/human_detector.hpp
@@ -1,15 +1,15 @@
 #ifndef HUMAN_DETECTOR_HPP
 #define HUMAN_DETECTOR_HPP
 
-#include <cstddef>
-#include <cstdint>
+#include <stddef.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define MODEL_BASE_PATH "../../models/"
-#define YOLO_MODEL_FILE "blaze.onnx"
+#define MODEL_FILE "blaze.onnx"
 
 typedef struct DetectionResult {
     uint8_t* jpeg_output;
@@ -19,14 +19,12 @@ typedef struct DetectionResult {
     int person_count;
 } DetectionResult;
 
-
 DetectionResult process_frame(
     const uint8_t* jpeg_data,
     size_t jpeg_len,
     int output_width,
     int output_height
 );
-
 
 void free_detection_result(DetectionResult* res);
 
